38e108a8d3c9eb54e84809a4324c230709aef5e9
Shadab Faiz <shadabfaiz607@gmail.com>
2025-05-31 16:58:28 +0530

    added spinner while checking ollama service running

--- mainServer/src/modules/ollama/helper.ts
+++ mainServer/src/modules/ollama/helper.ts
@@ -9,10 +9,23 @@ export class HelperService {
   constructor(private readonly httpService: HttpService) {}
 
   async checkOllamaStatus(baseUrl: string): Promise<boolean> {
+    const spinner = ['\\', '|', '/', '-'];
+    let i = 0;
+    let interval: NodeJS.Timeout | null = null;
     try {
       const tagsUrl = `${baseUrl}/api/tags`;
       this.logger.log(`Checking if Ollama is running at ${tagsUrl}`);
-      const response = await lastValueFrom(this.httpService.get(tagsUrl, { 'timeout': 5000 }));
+      const interval = setInterval(() => {
+        process.stdout.write(`\rChecking Ollama status... ${spinner[i]}`);
+        i = (i + 1) % spinner.length;
+      }, 100);
+      const response = await lastValueFrom(
+        this.httpService.get(tagsUrl, { timeout: 5000 }),
+      );
+      if (interval) {
+        clearInterval(interval);
+      }
+      process.stdout.write('\r');
       if (response.status === 200) {
         this.logger.log('Ollama is live.');
         return true;
@@ -23,6 +36,10 @@ export class HelperService {
         return false;
       }
     } catch (error) {
+      if (interval) {
+        clearInterval(interval);
+      }
+      process.stdout.write('\r'); // Clear the spinner
       this.logger.error('Ollama service is not live.', error.message);
       return false;
     }
