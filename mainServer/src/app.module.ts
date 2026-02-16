4c1b088afa3a9ac11a30f42995f2f55b371b8554
Shadab Faiz <shadab.faiz@myzeller.com>
2026-02-16 09:28:32 +0530

    commented out ollam module as it is not being used

--- mainServer/src/app.module.ts
+++ mainServer/src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GlobalModule } from './global.module';
 import { GeminiModule } from './modules/gemini/gemini.moduel';
-import { OllamaModule } from './modules/ollama/ollama.moduel';
 import { ToolsModule } from './modules/tools/tools.module';
 import { VoiceChatModule } from './modules/voiceChat/voice-chat.moduel';
 
 @Module({
   imports: [
     ToolsModule,
-    OllamaModule,
+    // OllamaModule,
     VoiceChatModule,
     GlobalModule,
     GeminiModule,
